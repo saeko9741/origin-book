cea699b8a49e6667111c77e31308c6588d9826f5
saeko9741 <saeco9741@gmail.com>
2020-02-23 07:19:15 +0000

    Add: 検索窓を追加

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,5 @@
 class HomesController < ApplicationController
   def top
-  	@user_name = current_user.name
   end
 
   def about
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,5 @@
 class SearchesController < ApplicationController
   def show
+  	@user_name = current_user.name
   end
 end
