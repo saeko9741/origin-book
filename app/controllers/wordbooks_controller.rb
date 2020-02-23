0ff9dbb3bd6b063643e1c1f78a6db622c3128946
saeko9741 <saeco9741@gmail.com>
2020-02-23 06:34:23 +0000

    Add: wordbooksコントローラ、アクション、ルーティング追加

---
+++ app/controllers/wordbooks_controller.rb
@@ -0,0 +1,18 @@
+class WordbooksController < ApplicationController
+
+	def index
+		
+	end
+	def crate
+		
+	end
+	def edit
+		
+	end
+	def update
+		
+	end
+	def destory
+		
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'searches/show'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'homes#top'
+  root 'searches#show'
   get 'homes/about'
+  get 'searches/show'
+  resources :wordbooks,only: [:index, :create, :edit, :update, :destroy]
 end
