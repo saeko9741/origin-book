dd1d3b2f4dd9299ca644d64cf7f914e14729778e
saeko9741 <saeco9741@gmail.com>
2020-02-27 06:57:00 +0000

    Change: search_cache_create_pathのhttpメソッドpostに変更

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homes#top'
   get 'homes/about' => 'homes#about'
-  get 'search_caches/create'
+  post 'search_caches/create'
   resources :wordbooks,only: [:new, :index, :create, :edit, :update, :destroy]
 end
