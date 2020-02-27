7c6b9b5e82ed18dba9bf604caa8b0d254205825e
saeko9741 <saeco9741@gmail.com>
2020-02-27 05:29:32 +0000

    Add: 英単語検索後、画像表示機能追加

--- app/controllers/wordbooks_controller.rb
+++ app/controllers/wordbooks_controller.rb
@@ -3,16 +3,22 @@ require 'json'
 class WordbooksController < ApplicationController
 
 	def new
-			url = 'https://pixabay.com/api/?key=15371520-8deb9c8dd8bbb01f97ab99cc1&q=flower&pretty=true'
-			uri = URI(url)
-			# ただの文字列ではなくurlと認識させる？
-			result = Net::HTTP.get(uri)
-			# 検索の結果を格納？
-			parsed_result = JSON.parse(result)
-			# JSONからRubyのハッシュをつくる
-			image_hits = parsed_result["hits"]
-			# ハッシュの中のhits(key)で要素(value)を取得する
-			@image_options = image_hits.map {|image| image["previewURL"]}
+  	########## 画像API ###############################################################
+  	word = params[:word]
+		url = 'https://pixabay.com/api/?key=15371520-8deb9c8dd8bbb01f97ab99cc1&q='+ word +'&pretty=true'
+		uri = URI(url)
+		# ただの文字列ではなくurlと認識させる？
+		result = Net::HTTP.get(uri)
+		# 検索の結果を格納？
+		parsed_result = JSON.parse(result)
+		# JSONからRubyのハッシュをつくる
+		image_hits = parsed_result["hits"]
+		# ハッシュの中のhits(key)で要素(value)を配列で取得
+		@image_options = image_hits.map {|image| image["previewURL"]}
+		# 配列内のハッシュが配列の要素となり、previewURL(key)を元にurlを要素分繰り返し処理し、配列で変数に格納される
+
+		############################################################################
+
 	end
 	def index
 		
