1b5602b59578afa4abc9462886b78e4b6d60c002
saeko9741 <saeco9741@gmail.com>
2020-03-02 14:23:28 +0000

    Add,Fix: wordbookに保存ができるようにcreateメソッド名修正、create, newアクション内、ストロングパラメータ追加

--- app/controllers/wordbooks_controller.rb
+++ app/controllers/wordbooks_controller.rb
@@ -1,14 +1,22 @@
 class WordbooksController < ApplicationController
 
 	def new
+		@wordbook = Wordbook.new
 		@search_cache = SearchCache.find(params[:search_cache_id])
 		@images = Image.where(search_cache_id: params[:search_cache_id])
 	end
 	def index
-		
 	end
-	def crate
-		
+	def create
+		wordbook = Wordbook.new(wordbook_params)
+		wordbook.user_id = current_user.id
+		if wordbook.save
+			redirect_to wordbooks_path
+		else
+			@search_cache = SearchCache.find(params[:search_cache_id])
+			@images = Image.where(search_cache_id: params[:search_cache_id])
+			render "new"
+		end
 	end
 	def edit
 		
@@ -18,5 +26,10 @@ class WordbooksController < ApplicationController
 	end
 	def destory
 		
+	end
+	private
+	def wordbook_params
+	  params.require(:wordbook).permit(:word, :meaning, :origin, :image_id)
+	  # definition, origin必要か
 	end
 end
