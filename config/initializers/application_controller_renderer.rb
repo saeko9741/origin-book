d6fed70a04c3165f2af0463fa514cc90c74057b2
saeko9741 <saeco9741@gmail.com>
2020-03-04 01:43:41 +0000

    Fix: refile secret keyエラーのためkeyを設定

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '4ee00d512f86bae32300efcf0314b14f0766bd6db2114af293dd0dded9ce6150a1852e8d6647b57718c0e5f86138d5d283918162e48f4cabc86b09a9baab3406'
