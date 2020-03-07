91d1630f6411cf53bfc7b6a67233c5e852501f50
saeko9741 <saeco9741@gmail.com>
2020-03-07 04:46:30 +0000

    Fix: エラー解消、スペース、コメント削除

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homes#top'
   get 'homes/about' => 'homes#about'
-  # リソースフルではないルーティング HTTPリクエスト URL, to: 'コントローラ名#アクション名' get homes/about, to 'homes#about'
   resources :wordbooks,except: [:show]
   resources :search_caches,only: [:create]
 end
