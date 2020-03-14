5c5fb5d2aff471ca3759cc4c5df27e4874cdd2f9
saeko9741 <saeco9741@gmail.com>
2020-03-14 07:23:15 +0000

    Change: バリデーション変更

--- app/models/wordbook.rb
+++ app/models/wordbook.rb
@@ -2,6 +2,6 @@ class Wordbook < ApplicationRecord
 	belongs_to :user
 	belongs_to :image
 	attachment :my_image
-  validates :meaning, presence: true, length: {maximum: 30, minimum: 2}
-  validates :origin, presence: true, length: {maximum: 200, minimum: 2}
+  validates :meaning, presence: true, length: {maximum: 30, minimum: 1}
+  validates :origin, presence: true, length: {maximum: 500, minimum: 2}
 end
