0629ca5c4feb3b2e02425490aac0ddadd68f31ab
saeko9741 <saeco9741@gmail.com>
2020-04-06 12:37:52 +0000

    Add: search_cache_spec 正常系

--- spec/models/search_cache_spec.rb
+++ spec/models/search_cache_spec.rb
@@ -1,3 +1,11 @@
 require 'rails_helper'
+
 RSpec.describe SearchCache, type: :model do
+  context "when a search_cache is valid" do
+    it "is valid with all data" do
+      search_cache = build(:search_cache)
+      expect(search_cache).to be_valid
+    end
+  end
+
 end
