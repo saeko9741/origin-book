d11dad8bc6761cf2ab8de2c86435c2c0d9e7af85
saeko9741 <saeco9741@gmail.com>
2020-04-22 14:15:33 +0000

    Update: user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,19 +8,42 @@ RSpec.describe User, type: :model do
     end
   end
   
-  # context "when a user is invalid" do
-  #   it "is invalid without a name" do
-  #   end
+  context "when a user is invalid" do
+    it "is invalid without a name" do
+      user = build(:user, name: " ")
+      expect(user).to_not be_valid
+    end
 
-  #   it "is invalid with a too short name" do
-  #   end
+    it "is invalid with a too short name" do
+      user = build(:user, name: "a")
+      expect(user).to_not be_valid
+    end
 
-  #   it "is invalid with a too long name" do
-  #   end
+    it "is invalid with a too long name" do
+      user = build(:user, name: "a" * 21)
+      expect(user).to_not be_valid
+    end
 
-  #   it "is invalid without an email" do
-  #   end
+    it "is invalid without an email" do
+      user = build(:user, email: " ")
+      expect(user).to_not be_valid
+    end
 
-  #   it "is"
-  # end
+    it "is invalid with without a password" do
+      password = " "
+      user = build(:user, password: password, password_confirmation: password)
+      expect(user).to_not be_valid
+    end
+
+    it "is invalid with a different password" do
+      user = build(:user, password: "aaa", password_confirmation: "bbb")
+      expect(user).to_not be_valid
+    end
+
+    it "is invalid with a too short password" do
+      user = build(:user, password: "a", password_confirmation: "a")
+      expect(user).to_not be_valid
+    end
+
+  end
 end
