466a28dd9b925bd8ee3f2ba3a55dbff0a234457b
saeko9741 <saeco9741@gmail.com>
2020-05-26 13:21:49 +0000

    Add: 一覧単語 非表示ボタン追加

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,5 +1,18 @@
-function changeVisible(){
-  var tbl = document.getElementsByClassName("target");
+function hiddenWord(){
+  var tbl = document.getElementsByClassName("word");
+  if(tbl[1].style.visibility == "visible"){
+     for(var i=0; i<tbl.length; i++){
+         tbl[i].style.visibility = "hidden";
+     }
+  }else{
+     for(var i=0; i<tbl.length; i++){
+         tbl[i].style.visibility = "visible";
+     }
+  }
+}
+
+function hiddenMeaning(){
+  var tbl = document.getElementsByClassName("meaning");
   if(tbl[1].style.visibility == "visible"){
      for(var i=0; i<tbl.length; i++){
          tbl[i].style.visibility = "hidden";
