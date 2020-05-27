91cd831801f12f99ec91a84468b478157318177b
saeko9741 <saeco9741@gmail.com>
2020-05-27 13:08:42 +0000

    Update: hidden button

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,6 +1,6 @@
 function hiddenWord(){
   var tbl = document.getElementsByClassName("word");
-  if(tbl[1].style.visibility == "visible"){
+  if(tbl[0].style.visibility == "visible"){
      for(var i=0; i<tbl.length; i++){
          tbl[i].style.visibility = "hidden";
      }
@@ -13,7 +13,7 @@ function hiddenWord(){
 
 function hiddenMeaning(){
   var tbl = document.getElementsByClassName("meaning");
-  if(tbl[1].style.visibility == "visible"){
+  if(tbl[0].style.visibility == "visible"){
      for(var i=0; i<tbl.length; i++){
          tbl[i].style.visibility = "hidden";
      }
