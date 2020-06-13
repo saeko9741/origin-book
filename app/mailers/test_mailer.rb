4f113e09d900f8a9ebe6ac9f4fbcd9c7de3e1efa
saeko9741 <saeco9741@gmail.com>
2020-06-13 14:51:47 +0000

    Update: edit mailer

--- app/mailers/test_mailer.rb
+++ app/mailers/test_mailer.rb
@@ -1,2 +1,9 @@
 class TestMailer < ApplicationMailer
+  default from: 'notifications@example.com'
+
+  def welcome_email
+    @user = params[:user]
+    @url = 'http://localhost:3000/users/sign_in'
+    mail(to: @user.email, subject: '私のサイトへようこそ')
+  end
 end
