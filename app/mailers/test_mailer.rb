a9d072ff6874c9b9426ab29043bb8395bfdcb370
saeko9741 <saeco9741@gmail.com>
2020-06-15 14:20:38 +0000

    Remove: url

--- app/mailers/test_mailer.rb
+++ app/mailers/test_mailer.rb
@@ -3,7 +3,7 @@ class TestMailer < ApplicationMailer
 
   def test_email
     @user = params[:user]
-    @url = 'http://localhost:3000/users/sign_in'
+    # @url = 'http://localhost:3000/users/sign_in'
     mail(to: @user.email, subject: '私のサイトへようこそ')
   end
 end
