9250565b2b923c602e21c8a0201a51da33ef2f5f
saeko9741 <saeco9741@gmail.com>
2020-06-24 05:32:35 +0000

    Delete: files

--- app/mailers/test_mailer.rb
+++
@@ -1,9 +0,0 @@
-class TestMailer < ApplicationMailer
-  default from: 'notifications@example.com'
-
-  def test_email
-    @user = params[:user]
-    @wordbook = params[:wordbook]
-    mail(to: @user.email, subject: '私のサイトへようこそ')
-  end
-end
